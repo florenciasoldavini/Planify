db08d89ccbe26b3f5c0beab8c9c6e36df492505c
Florencia <florenciasoldavinivarela@gmail.com>
2024-04-03 00:01:04 -0300

    changes to navbar & serviceselection

--- src/scenes/ServiceSelection.jsx
+++ src/scenes/ServiceSelection.jsx
@@ -36,11 +36,11 @@ function ServiceSelection() {
                                         xmlns="http://www.w3.org/2000/svg"
                                         fill="none"
                                         viewBox="0 0 24 24"
-                                        stroke-width="2.5"
+                                        strokeWidth="2.5"
                                         stroke="currentColor"
                                         class="w-6 h-6">
-                                        <path stroke-linecap="round"
-                                            stroke-linejoin="round"
+                                        <path strokeLinecap="round"
+                                            strokeLinejoin="round"
                                             d="M5 12h14" />
                                     </svg>
                                 ) : (
